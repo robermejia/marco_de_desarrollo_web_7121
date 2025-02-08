b22e2cac96ad41ad614a0d728c2289ec301b7f11
Roberto Mejía <59416763+robermejia@users.noreply.github.com>
2025-02-07 21:33:58 -0500

    semana 5 sesion 2

---
+++ semana_5/sesion_2/clase/spar-spring/src/main/java/com/robermejia/spar_spring/SparSpringApplication.java
@@ -0,0 +1,13 @@
+package com.robermejia.spar_spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SparSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SparSpringApplication.class, args);
+	}
+
+}
